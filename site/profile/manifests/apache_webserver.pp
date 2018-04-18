0b1d4a680d1b40b536b55e46b5c70e3e3b47cb90
Simon Anderson <simon.anderson111@gmail.com>
2018-04-18 14:05:53 +0800

    added dir

--- site/profile/manifests/apache_webserver.pp
+++ site/profile/manifests/apache_webserver.pp
@@ -3,11 +3,11 @@ class profile::apache_webserver {
 
   include apache
 
-  file {'/var/www':
+  file {['/var','/var/www','/var/www/html']:
     ensure => directory,
   }
 
-  file {'/var/www':
+  file {'/var/www/html/index.html':
     ensure  => file,
     content => epp('profile/index.html.epp'),
   }
